8abe5786b3bb4687b32082a818d8895ff9b442eb
Klement Matej (211229) <xkleme14@vutbr.cz>
2022-03-15 14:49:30 +0100

    build

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd ./build
-xc8 --chip=16f917 --warn=1 ../main.c
+xc8 --chip=16f917 ../main.c ../uart.c
 
 pause
--- build/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cd ./build
-xc8 --chip=16f917 ../main.c ../uart.c
-
-pause
